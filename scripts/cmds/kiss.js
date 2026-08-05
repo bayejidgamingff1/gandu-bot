15316af282e5c63fc5e16088ef599e7f95fe6974
bayejidgamingff1 <bayejidgamingff1@gmail.com>
2026-08-05 11:11:45 +0600

    Update kiss.js

--- scripts/cmds/kiss.js
+++ scripts/cmds/kiss.js
@@ -7,8 +7,8 @@ module.exports = {
     config: {
         name: "kiss",
         aliases: ["kiss"],
-        version: "1.0",
-        author: "♡︎ 𝐻𝐴𝑆𝐴𝑁 ♡︎",
+        version: "1.1",
+        author: "♡︎ BAYEJID ♡︎",
         countDown: 5,
         role: 0,
         shortDescription: "KISS",
@@ -19,19 +19,28 @@ module.exports = {
 
     onStart: async function ({ api, message, event, usersData }) {
         const uid = event.senderID;
-        const mention = Object.keys(event.mentions);
-        const uid1 = Object.keys(event.mentions)[0];
-        const uid2 = event.messageReply ? event.messageReply.senderID : null;
-        const uids = uid1 || uid2;
-           if (!uids) return message.reply("😘 | Tag or reply to someone you want to kiss.");
-             let two = uid, one = uids;
-      
-              if (mention.length === 2) {
-                     one = mention[1];
-                     two = mention[0];
-              }
+        const mentions = Object.keys(event.mentions || {});
+        let one, two;
 
- 
+        // ২ জন কে ট্যাগ করলে
+        if (mentions.length >= 2) {
+            one = mentions[0];
+            two = mentions[1];
+        } 
+        // ১ জন কে ট্যাগ করলে
+        else if (mentions.length === 1) {
+            one = mentions[0];
+            two = uid; // যে কমান্ড দিয়েছে সে নিজে ২য় জন
+        } 
+        // রিপ্লাই দিলে
+        else if (event.messageReply) {
+            one = event.messageReply.senderID;
+            two = uid;
+        } 
+        // ট্যাগ বা রিপ্লাই না থাকলে
+        else {
+            return message.reply("😘 | Tag or reply to someone you want to kiss.");
+        }
 
         try {
             const avatarURL1 = await usersData.getAvatarUrl(one);
@@ -45,7 +54,7 @@ module.exports = {
             const avatar1 = await Jimp.read((await axios({ url: avatarURL1, responseType: "arraybuffer" })).data);
             const avatar2 = await Jimp.read((await axios({ url: avatarURL2, responseType: "arraybuffer" })).data);
             
-            // ব্যাকগ্রাউন্ড সেট করা (কাস্টম ব্যাকগ্রাউন্ড ইউআরএল)
+            // ব্যাকগ্রাউন্ড সেট করা
             const background = await Jimp.read("https://i.imgur.com/pLubFCh.jpeg");
 
             // ব্যাকগ্রাউন্ড রিসাইজ
@@ -55,22 +64,31 @@ module.exports = {
             avatar1.resize(110, 110).circle();
             avatar2.resize(110, 110).circle();
             
-            background.composite(avatar1, 100, 130);  // প্রথম ইউজারের ইমেজ বসানো
-            background.composite(avatar2, 250, 100);  // দ্বিতীয় ইউজারের ইমেজ বসানো
+            background.composite(avatar1, 100, 130);  // প্রথম ইউজার (যাকে ট্যাগ করা হয়েছে)
+            background.composite(avatar2, 250, 100);  // দ্বিতীয় ইউজার (যে কমান্ড দিয়েছে)
             
-            // ফাইল সেভ করা
-            const imagePath = path.join(__dirname, "tmp", `${one}_${two}_kiss.png`);
+            // ফাইল সেভ করার ফোল্ডার নিশ্চিত করা
+            const tmpDir = path.join(__dirname, "tmp");
+            if (!fs.existsSync(tmpDir)) {
+                fs.mkdirSync(tmpDir, { recursive: true });
+            }
+
+            const imagePath = path.join(tmpDir, `${one}_${two}_kiss.png`);
             await background.writeAsync(imagePath);
 
-            // পাঠানো
-            message.reply({
+            // ফাইল পাঠানো ও ডিলিট করা
+            return message.reply({
                 body: "Ummmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmaaaaaaaaaaaaaaaaaaahhhhhhhhh bbz 😘😽🥵",
                 attachment: fs.createReadStream(imagePath)
-            }, () => fs.unlinkSync(imagePath));
+            }, () => {
+                if (fs.existsSync(imagePath)) {
+                    fs.unlinkSync(imagePath);
+                }
+            });
             
         } catch (error) {
             console.error(error);
-            message.reply("Something went wrong while generating the image.");
+            return message.reply("Something went wrong while generating the image.");
         }
     }
 };
